8a98b1fb19fe519b965bb4ade14d967e6ec3cb47
mhumesf <mhumesf@gmail.com>
2014-07-02 12:32:28 -0700

    Just playin

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    $('input[type=radio]').on('change', function(){
+
+    });
+
+    var show = $('#3TTotal').val()
+
+    $("#btnSubmit").click(function(){
+        var qual_d = parseFloat($('#qual_d').val())
+        var mqual_d = parseFloat($('#mqual_d').val())
+        var nqual_d = parseFloat($('#nqual_d').val())
+        var Sswipe = parseFloat($('#Sswipe').val())
+        var Skey = parseFloat($('#Skey').val())
+        var Pswipe = parseFloat($('#Pswipe').val())
+        var Pkey = parseFloat($('#Pkey').val())
+        var show = parseFloat($('#3TTotal'))
+
+    $('#3TTotal').val(($('#qual_d').val() * ($('#qual').val() / 100)) + ($('#mqual_d').val() * ($('#mqual').val()) / 100) + ($('#nqual_d').val() * ($('#nqual').val() / 100)));
+    $('#STotal').val((qual_d * (Sswipe/100)) + ((mqual_d + nqual_d) * (Skey / 100)));
+    $('#PTotal').val((qual_d * (Pswipe/100)) + ((mqual_d + nqual_d) * (Pkey / 100))).toFixed(2);
+
+    });
+});
