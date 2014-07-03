b0ee299c1afc59d9a1d3787adea9a7b3c300b7d1
mhumesf <mhumesf@gmail.com>
2014-07-02 21:44:13 -0700

    For realz

--- js/main.js
+++ js/main.js
@@ -38,32 +38,6 @@ $(document).ready(function() {
             console.log("option5");
         };
 
-<<<<<<< HEAD
-=======
-        var alpha = ($('#qual_d').val() * ($('#qual').val() / 100)) + ($('#mqual_d').val() * ($('#mqual').val()) / 100) + ($('#nqual_d').val() * ($('#nqual').val() / 100));
-        var beta = (qual_d * (Sswipe/100)) + ((mqual_d + nqual_d) * (Skey / 100));
-        var gamma = qual_d * (Pswipe/100)) + ((mqual_d + nqual_d) * (Pkey / 100))).toFixed(2);
-
-        $(".radio").click(function(){
-            if($(this).hasClass("radio1")){
-                console.log("radio1")
-            }
-            if($(this).hasClass("radio2")){
-                console.log("radio2")
-            }
-            if($(this).hasClass("radio3")){
-                console.log("radio3")
-            }
-            if($(this).hasClass("radio4")){
-                console.log("radio4")
-            }
-            if($(this).hasClass("radio5")){
-                console.log("radio5")
-            }
-
-        }
-
->>>>>>> 5deac6dff96de5d13ee0e6b9280a63455c8f12ff
         $('#3TTotal').val(alpha*n);
         $('#STotal').val(beta*n);
         $('#PTotal').val(gamma*n);
