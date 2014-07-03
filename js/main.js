5deac6dff96de5d13ee0e6b9280a63455c8f12ff
finn <finn@finns-Mac-Pro.local>
2014-07-02 20:05:00 -0700

    I has all the cheese

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-    $('input[type=radio]').on('change', function(){
-
-    });
 
     var show = $('#3TTotal').val()
 
@@ -16,9 +13,31 @@ $(document).ready(function() {
         var Pkey = parseFloat($('#Pkey').val())
         var show = parseFloat($('#3TTotal'))
 
-    $('#3TTotal').val(($('#qual_d').val() * ($('#qual').val() / 100)) + ($('#mqual_d').val() * ($('#mqual').val()) / 100) + ($('#nqual_d').val() * ($('#nqual').val() / 100)));
-    $('#STotal').val((qual_d * (Sswipe/100)) + ((mqual_d + nqual_d) * (Skey / 100)));
-    $('#PTotal').val((qual_d * (Pswipe/100)) + ((mqual_d + nqual_d) * (Pkey / 100))).toFixed(2);
+        var alpha = ($('#qual_d').val() * ($('#qual').val() / 100)) + ($('#mqual_d').val() * ($('#mqual').val()) / 100) + ($('#nqual_d').val() * ($('#nqual').val() / 100));
+        var beta = (qual_d * (Sswipe/100)) + ((mqual_d + nqual_d) * (Skey / 100));
+        var gamma = qual_d * (Pswipe/100)) + ((mqual_d + nqual_d) * (Pkey / 100))).toFixed(2);
 
-    });
+        $(".radio").click(function(){
+            if($(this).hasClass("radio1")){
+                console.log("radio1")
+            }
+            if($(this).hasClass("radio2")){
+                console.log("radio2")
+            }
+            if($(this).hasClass("radio3")){
+                console.log("radio3")
+            }
+            if($(this).hasClass("radio4")){
+                console.log("radio4")
+            }
+            if($(this).hasClass("radio5")){
+                console.log("radio5")
+            }
+
+        }
+
+        $('#3TTotal').val(alpha*n);
+        $('#STotal').val(beta*n);
+        $('#PTotal').val(gamma*n);
+
 });
